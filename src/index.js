968f202c4d76bf7ea7574ac63d57d03d0db78cbb
JordanBeziau <jordanbeziau@gmail.com>
2017-07-08 20:54:28 +0200

    Initials setup

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-  
+  const canvas = document.getElementById("canvas"),
+    context = canvas.getContext("2d");
+
+  context.fillRect(10, 10, 100, 100);
 
 });
